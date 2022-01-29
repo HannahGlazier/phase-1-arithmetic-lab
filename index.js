16454612b428e7ab12ea779e2a5e1c6f4ba234c4
Hannah Glazier <hrayglazier@aol.com>
2022-01-29 11:55:04 -0700

    commit tree 5c3ec65432256138544a6add5fb0bb5cef5173c7

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+//Multiply 
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+//Random number generator 
+
+let random = Math.floor(Math.random() + 1);
+
+// remainder
+const num3 = 19;
+const num4 = 5;
+let mod = num3 % num4;
+
+//20 max set 
+let max = Math.max(20);
